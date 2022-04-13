483872798afd430dd4fb26629c258f2c6fcf5c35
Max Flores <max@maxflores.dev>
2022-04-12 23:02:40 -0600

    Navbar and Footer

--- src/comp/Footer.jsx
+++ src/comp/Footer.jsx
@@ -1,9 +1,16 @@
-
+import logoSquare from '../img/logos/tg-logo-2.svg'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <div className="bg-stone-200 px-4">
+            <div className="container grid grid-cols-4 gap-16 py-16 justify-between mx-auto">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minima quaerat harum incidunt aliquam recusandae nihil repellendus dolores magni fugiat.</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minima quaerat harum incidunt aliquam recusandae nihil repellendus dolores magni fugiat.</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minima quaerat harum incidunt aliquam recusandae nihil repellendus dolores magni fugiat.</p>
+				<img src={logoSquare} alt="Logo Square" className='w-32 place-self-end'/>
+			</div>
+        </div>
+    );
+};
 
-export default Footer
+export default Footer;
--- src/comp/Navbar.jsx
+++ src/comp/Navbar.jsx
@@ -1,33 +1,39 @@
 import { Link } from 'react-router-dom';
+import logo from '../img/logos/tg-logow-1.svg';
 
 const Navbar = () => {
-    const links = 'text-xl font-semibold text-white p-2 hover:bg-oragnge-600';
+    const links = 'text-xl text-white p-2 hover:bg-oragnge-600';
 
     return (
-        <div className="bg-orange-500">
+        <div className="bg-black">
             <div className="container mx-auto py-12">
-                <div className="flex justify-between">
-                    <Link className={links} to="/">
-                        Home
+                <div className="flex justify-between mx-4">
+                    <Link to="/">
+                        <img src={logo} alt="Logo" className="w-40" />
                     </Link>
-                    <Link className={links} to="/artists">
-                        Artist
-                    </Link>
-                    <Link className={links} to="/artworks">
-                        Artwork
-                    </Link>
-                    <Link className={links} to="/about">
-                        About
-                    </Link>
-                    <Link className={links} to="/gallery">
-                        Gallery
-                    </Link>
-                    <Link className={links} to="/login">
-                        login
-                    </Link>
-                    <Link className={links} to="/contact">
-                        Contact
-                    </Link>
+                    <div className="md:flex gap-4 items-center hidden">
+                        <Link className={links} to="/artists">
+                            artist
+                        </Link>
+                        <Link className={links} to="/artworks">
+                            artwork
+                        </Link>
+                        <Link className={links} to="/about">
+                            about
+                        </Link>
+                        <Link className={links} to="/gallery">
+                            gallery
+                        </Link>
+                        <Link className={links} to="/login">
+                            login
+                        </Link>
+                        <Link className={links} to="/contact">
+                            contact
+                        </Link>
+                    </div>
+                    <div className="flex items-center md:hidden">
+                        <h1 className='text-white text-xl cursor-pointer'>menu</h1>
+                    </div>
                 </div>
             </div>
         </div>
